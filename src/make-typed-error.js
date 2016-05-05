e8513c0936b70071e0d8e05653f245e900d7490e
Guillaume Marceau <guillaume.marceau@bodylabs.com>
2016-05-05 15:51:51 -0400

    Fix tests

--- src/make-typed-error.js
+++ src/make-typed-error.js
@@ -1,13 +1,17 @@
+var _ = require('underscore');
+
 var makeTypedError = function (name, Supertype) {
 
     Supertype = Supertype || Error;
 
     var TypedError = function (message) {
+        var sup = new Supertype(message);
+        _(this).extend(sup);
         this.name = name;
         this.message = message;
-        var tempStack = (new Supertype()).stack;
+
         // replace 'Error' with actual name in stack trace
-        this.stack = tempStack.replace(/^[^:]+:/, name + ':');
+        this.stack = sup.stack.replace(/^[^:]+:/, name + ':');
     };
 
     TypedError.prototype = Object.create(Supertype.prototype);
